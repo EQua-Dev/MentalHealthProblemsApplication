3dc03fe15514bb8045714cb22f88c6d0ef44b680
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-08-06 14:51:08 +0100

    client auth done

---
+++ app/src/main/java/com/androidstrike/schoolprojects/mentalhealthproblemsapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidstrike.schoolprojects.mentalhealthproblemsapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
