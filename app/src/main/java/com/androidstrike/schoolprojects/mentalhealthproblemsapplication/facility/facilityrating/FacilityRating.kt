dfd59ce0b39be1f80ab4b386853eac4b60671244
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-09-12 16:26:40 +0100

    implemented updates

--- app/src/main/java/com/androidstrike/schoolprojects/mentalhealthproblemsapplication/facility/facilityrating/FacilityRating.kt
+++ app/src/main/java/com/androidstrike/schoolprojects/mentalhealthproblemsapplication/facility/facilityrating/FacilityRating.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +18,7 @@ import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.databind
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.model.Client
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.model.ClientFeedBack
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.model.Service
+import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.model.ServiceType
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.utils.Common
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.utils.hideProgress
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.utils.showProgress
@@ -22,6 +26,7 @@ import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.utils.to
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.iarcuschin.simpleratingbar.SimpleRatingBar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -101,6 +106,10 @@ class FacilityRating : Fragment() {
                     Log.d("TAG", "ratings: ${model.ratings.toFloat().toInt()}")
                     holder.serviceRatingText.text = model.feedBackText
 
+                    holder.ratingDetailButton.setOnClickListener {
+                        launchRatingsDetailDialog(model)
+                    }
+
                 }
 
             }
@@ -111,6 +120,67 @@ class FacilityRating : Fragment() {
         facilityRatingAdapter?.startListening()
         binding.rvFacilityClientsRating.adapter = facilityRatingAdapter
     }
+
+    private fun launchRatingsDetailDialog(model: ClientFeedBack) {
+
+        val builder =
+            layoutInflater.inflate(
+                R.layout.custom_facility_rating_detail_dialog
+                ,
+                null
+            )
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(builder)
+            .setCancelable(false)
+            .create()
+
+        val tvClientName =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_customer_name)
+        val tvChosenServiceName =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_chosen_service_name)
+        val tvChosenServiceType =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_chosen_service_type)
+        val tvRatingStars =
+            builder.findViewById<SimpleRatingBar>(R.id.txt_facility_rating_stars)
+        val tvRatingsText =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_text)
+        val tvRequestedDateCreated =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_date_created)
+        val tvRequestedTimeCreated =
+            builder.findViewById<TextView>(R.id.facility_rating_detail_time_created)
+        val btnAcceptRequest =
+            builder.findViewById<Button>(R.id.btn_facility_rating_okay)
+
+        val client = getCustomer(model.customerID)!!
+        val service = getService(model.organisationProfileService)!!
+        val serviceType = getServiceType(model.serviceType)!!
+
+        tvClientName.text = resources.getString(
+            R.string.requesting_client_name,
+            client.customerFirstName,
+            client.customerLastName
+        )
+        tvChosenServiceName.text =
+            resources.getString(R.string.requesting_chosen_service_name, service.serviceName)
+        tvChosenServiceType.text = resources.getString(
+            R.string.requesting_chosen_service_type,
+            serviceType.serviceTypeName
+        )
+        tvRatingsText.text = model.feedBackText
+        tvRequestedDateCreated.text =
+            resources.getString(R.string.requesting_date_created, model.dateCreated)
+        tvRatingStars.numberOfStars = model.ratings.toFloat().toInt()
+        tvRequestedTimeCreated.text =
+            resources.getString(R.string.requesting_time_created, model.timeCreated)
+        btnAcceptRequest.setOnClickListener {
+         dialog.dismiss()
+
+        }
+
+        dialog.show()
+
+    }
+
     private fun getCustomer(customerId: String): Client? {
         requireContext().showProgress()
         val deferred = CoroutineScope(Dispatchers.IO).async {
@@ -157,6 +227,32 @@ class FacilityRating : Fragment() {
 
         return service
     }
+
+    private fun getServiceType(serviceTypeId: String): ServiceType? {
+        requireContext().showProgress()
+        val deferred = CoroutineScope(Dispatchers.IO).async {
+            try {
+                val snapshot =
+                    Common.servicesTypeCollectionRef.document(serviceTypeId).get().await()
+                if (snapshot.exists()) {
+                    return@async snapshot.toObject(ServiceType::class.java)
+                } else {
+                    return@async null
+                }
+            } catch (e: Exception) {
+                Handler(Looper.getMainLooper()).post {
+                    requireContext().toast(e.message.toString())
+                }
+                return@async null
+            }
+        }
+
+        val serviceType = runBlocking { deferred.await() }
+        com.androidstrike.schoolprojects.mentalhealthproblemsapplication.utils.hideProgress()
+
+        return serviceType
+    }
+
 
 
     override fun onDestroyView() {
--- app/src/main/java/com/androidstrike/schoolprojects/mentalhealthproblemsapplication/facility/facilityrating/FacilityRatingAdapter.kt
+++ app/src/main/java/com/androidstrike/schoolprojects/mentalhealthproblemsapplication/facility/facilityrating/FacilityRatingAdapter.kt
@@ -1,6 +1,7 @@
 package com.androidstrike.schoolprojects.mentalhealthproblemsapplication.facility.facilityrating
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.androidstrike.schoolprojects.mentalhealthproblemsapplication.R
@@ -14,7 +15,7 @@ class FacilityRatingAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
     var serviceName: TextView
     var serviceRating: SimpleRatingBar
     var serviceRatingText: TextView
-    //var rateServiceButton: Button
+    var ratingDetailButton: Button
 
 
     init {
@@ -24,7 +25,7 @@ class FacilityRatingAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
         serviceName = itemView.findViewById(R.id.txt_facility_rated_service_name)
         serviceRating = itemView.findViewById(R.id.txt_client_service_rating)
         serviceRatingText = itemView.findViewById(R.id.txt_facility_rated_text)
-        //rateServiceButton = itemView.findViewById(R.id.btn_client_rate_invoice_generated)
+        ratingDetailButton = itemView.findViewById(R.id.btn_facility_view_ratings_details)
 
 
 
